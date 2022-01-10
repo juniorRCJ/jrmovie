37d1e3df73e0314ca300bd5de2711c75af0c8208
juniorRCJ <caldeira.junior@gmail.com>
2022-01-10 16:37:12 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,23 @@
+
+import { ReactComponent as GithubIcon } from 'assets/img/git.svg'
+import './styles.css'
+
+function Navbar() {
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>JRMovie</h1>
+                    <a href="https://github.com/juniorRCJ">
+                        <div className="dsmovie-contact-container">
+                            <GithubIcon />
+                            <p className="dsmovie-contact-link">/juniorRCJ</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
