9f6d83e1a80fb4c0d25097adda221067ba78fcc2
juniorRCJ <caldeira.junior@gmail.com>
2022-01-10 11:26:53 -0300

    Project Created

---
+++ backend/src/main/java/com/juniorcaldeira/jrmovie/JrmovieApplication.java
@@ -0,0 +1,13 @@
+package com.juniorcaldeira.jrmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JrmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/juniorcaldeira/jrmovie/JrmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juniorcaldeira.jrmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JrmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
